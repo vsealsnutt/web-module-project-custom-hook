34479a18371acc33d5000873715e7ffa66ad80d6
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-19 13:53:46 -0500

    built the useDarkMode hook and replaced the darkMode state definition with useDarkMode in App.js

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,1 +1,9 @@
+import { useState } from 'react';
 import { useLocalStorage } from './useLocalStorage';
+
+const useDarkMode = (initalValue) => {
+    const [darkMode, setDarkMode] = useState(initalValue);
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
